23c7077e479866d88d9c2028847a3931045a66ff
home <62454927+minjunasdf@users.noreply.github.com>
2023-05-27 01:22:01 +0900

    변경

--- main.py
+++ main.py
@@ -1,52 +1,62 @@
 import matplotlib.pyplot as plt
+import random as rd
 
-t = 0
-v = 0
-h = 0  # 최고 높이가 가장 높은게 목표
 
-dt = 0.1
+dt = 0.01
 g = 9.8
-m1 = 10
-m2 = 10
-m3 = 10
-ml1 = 10
-ml2 = 10
-ml3 = 10
-a = -30  # 분사 속도
-u = 10  # 상대 속도
-
 height = []
 
 
-def sim(simv, simh, simm, simml, ison):
+def sim(simv, simh, simm, simml, a, ison):
     resolh = simh + simv * dt
     if ison:
-        resolv = simv - (g + u * a / (simm + simml)) * dt  # 분사할 때
-        resolml = simml + a * dt
+        resolv = simv - (g - a * a / 3 * (simm + simml)) * dt  # 분사할 때
+        resolml = simml - a * dt
         return resolv, resolh, resolml
     else:
         resolv = simv - g * dt  # 분사 안할 때
         return resolv, resolh, simml
 
 
-while v >= 0:
-    if ml1 > 0:
-        print(t, sim(v, h, m1 + m2 + m3 + ml2 + ml3, ml1, True))
-        v, h, ml1 = sim(v, h, m1, ml1, True)
-        height.append(h)
-    elif ml2 > 0:
-        print(t, sim(v, h, m2 + m3 + ml3, ml2, True))
-        v, h, ml2 = sim(v, h, m1, ml2, True)
-        height.append(h)
-    elif ml3 > 0:
-        print(t, sim(v, h, m3, ml3, True))
-        v, h, ml3 = sim(v, h, m1, ml3, True)
-        height.append(h)
-    else:
-        print(t, sim(v, h, m3, ml3, False))
-        v, h, ml3 = sim(v, h, m1, ml3, False)
-        height.append(h)
-    t = (t + 1)
+def generate_population(size=10):
+    population = []
+
+    for j in range(size):
+        gene = []
+        for i in range(9):
+            gene.append(rd.randrange(1,30))
+        population.append(gene)
+
+    return population
+
+
+def fitness(gene):
+    t, v, h = 0, 0, 0
+    m1, m2, m3, ml1, ml2, ml3, a1, a2, a3 = gene
+
+    while v >= 0:
+        if ml1 > a1 * dt:
+            print(t, sim(v, h, m1 + m2 + m3 + ml2 + ml3, ml1, a1, True))
+            v, h, ml1 = sim(v, h, m1, ml1, a1, True)
+            height.append(h)
+        elif ml2 > a2 * dt:
+            print(t, sim(v, h, m2 + m3 + ml3, ml2, a2, True))
+            v, h, ml2 = sim(v, h, m1, ml2, a2, True)
+            height.append(h)
+        elif ml3 > a3 * dt:
+            print(t, sim(v, h, m3, ml3, a3, True))
+            v, h, ml3 = sim(v, h, m1, ml3, a3, True)
+            height.append(h)
+        else:
+            print(t, sim(v, h, m3, ml3, a3, False))
+            v, h, ml3 = sim(v, h, m1, ml3, a3, False)
+            height.append(h)
+        t = (t + 1)
+
+
+pop = generate_population()
+fitness(pop[0])
+print(pop[0])
 
 plt.plot(range(len(height)), height, 'b')
 plt.grid()
